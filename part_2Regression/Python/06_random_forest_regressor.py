c26cf3e9711c13066d5ed4757e16b6953bb2b5d4
qalmaqihir <supine.me@gmail.com>
2022-08-14 13:06:07 +0600

    Regression part implemented in python (end)

---
+++ part_2Regression/Python/06_random_forest_regressor.py
@@ -0,0 +1,28 @@
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('/home/qalmaqihir/BreakGojalti/current/codes_for_books/Machine Learning A-Z (Codes and Datasets)/Part 2 - Regression/Section 7 - Support Vector Regression (SVR)/Python/Position_Salaries.csv')
+X= dataset.iloc[:,1:-1].values
+y=dataset.iloc[:,-1].values
+
+# Training the random forest regressor on the whole dataset
+from sklearn.ensemble import RandomForestRegressor
+regressor = RandomForestRegressor(n_estimators=10)
+regressor.fit(X,y)
+
+# Predicting
+regressor.predict([[6.5]])
+
+# Visualizing the results
+X_grid = np.arange(min(X), max(X), 0.01)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X, y, color = 'red')
+plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
+plt.title('Truth or Bluff (Random Forest Regression)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
