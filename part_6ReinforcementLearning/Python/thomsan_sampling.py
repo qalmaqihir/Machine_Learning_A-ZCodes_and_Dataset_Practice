86c3bf7d6787d0c4e1b714994780d2815df1a4d9
qalmaqihir <supine.me@gmail.com>
2022-09-08 12:08:05 +0600

    Reinforcement Learning: Thomson Sampling Algo

--- part_6ReinforcementLearning/Python/thomsan_sampling.py
+++ part_6ReinforcementLearning/Python/thomsan_sampling.py
@@ -1,6 +1,6 @@
-# Upper Confidence Bound (UCB)
-import math
+# Thomsan Sampling
 
+import math
 # Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
@@ -11,34 +11,28 @@ dataset_path="/home/qalmaqihir/BreakGojalti/current/codes_for_books/Machine Lear
 dataset = pd.read_csv(dataset_path)
 print(dataset)
 
-# Implementing UCB
-# N=10000
+# Implementing Thomsan Sampling
+import random
 N=50
 d=10
-
 ads_selected=[]
-number_of_selection=[0]*d
-sums_of_rewards=[0]*d
+numbers_rewards_1=[0]*d
+numbers_of_rewards_0=[0]*d
 total_reward=0
-
 for n in range(0,N):
     ad=0
-    max_upper_bound=0
+    max_random=0
     for i in range(0,d):
-        if number_of_selection[i] >0:
-            average_reward=sums_of_rewards[i]/number_of_selection[i]
-            delta_i = math.sqrt(3/2*math.log(n+1)/number_of_selection[i])
-            upper_bound=average_reward+delta_i
-        else:
-            upper_bound=1e400
-        if upper_bound>max_upper_bound:
-            max_upper_bound=upper_bound
+        random_beta=random.betavariate(numbers_of_rewards_0[i]+1,numbers_rewards_1[i]+1)
+        if random_beta>max_random:
+            max_random=random_beta
             ad=i
-
     ads_selected.append(ad)
-    number_of_selection[ad]+=1
-    reward=dataset.values[n,ad]
-    sums_of_rewards[ad]+=reward
+    reward = dataset.values[n, ad]
+    if reward==1:
+        numbers_rewards_1[ad]+=1
+    else:
+        numbers_of_rewards_0[ad]+=1
     total_reward+=reward
 
 
@@ -48,7 +42,4 @@ plt.title('Histogram of ad selections')
 plt.xlabel('Ads')
 plt.ylabel('Number of time each ad was selected')
 plt.show()
-
-
-
 
