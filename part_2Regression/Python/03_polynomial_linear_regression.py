9564a9c26b47af3f4d4a8bdd437804b32be29361
qalmaqihir <supine.me@gmail.com>
2022-09-13 13:47:54 +0600

    refactoring regression

--- part_2Regression/Python/03_polynomial_linear_regression.py
+++ part_2Regression/Python/03_polynomial_linear_regression.py
@@ -15,7 +15,7 @@ y=dataset.iloc[:,-1].values
 print(X)
 print(y)
 # # Splitting the dataset into training and testing set
-# from sklearn.model_selection import train_test_split
+# from sklearn.part_10model_selection import train_test_split
 # X_train, X_test, y_train,y_test = train_test_split(X,y,test_size=1/3,random_state=0)
 
 # Training the Linear Regression model on the whole dataset
